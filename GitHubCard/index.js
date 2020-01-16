dbc2feb0fe64103241d565cbfbe184da3e1f8e4b
carlsachs <sachscarl@gmail.com>
2020-01-16 17:18:16 -0500

    added elements and appends

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,11 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get("https://api.github.com/users/carlsachs")
+  .then(response => {
+  console.log(response);
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -26,6 +31,34 @@
 
 const followersArray = [];
 
+
+function (obj){
+
+  const card = document.createElement('div');
+  const img = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  card.append(img)
+  card.append(cardInfo)
+  cardInfo.append(name)
+  cardInfo.append(username)
+  cardInfo.append(location)
+  cardInfo.append(profile)
+  cardInfo.append(followers)
+  cardInfo.append(following)
+  cardInfo.append(bio)  
+
+
+
+
+}
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
